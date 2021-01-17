d7cbb01ca127bfe7440a107ea6eda5259765e745
SantosMaycon <mayconalves.santos14@gmail.com>
2021-01-17 19:20:56 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <div>
+          <span>Make with ❤️ for the PokéSpartans team Platzi Master - Developed with ❤️ by @SantosMaycon</span>
+        </div>
+        <span>
+          Ours Team
+        </span>
+    </footer>
+  )
+}
+
+export default Footer
