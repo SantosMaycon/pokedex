311760ca9e5908fece189bd257c137147f1dff23
SantosMaycon <mayconalves.santos14@gmail.com>
2021-02-20 22:42:44 -0300

    fix import 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import './App.css'
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Footer from './Components/Footer';
-import SearchPokemons from './Components/SearchPokemons';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/index";
+import Home from "./Components/Home/index";
+import Footer from "./Components/Footer/index";
+import SearchPokemons from "./Components/SearchPokemons/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
         <Footer />
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
