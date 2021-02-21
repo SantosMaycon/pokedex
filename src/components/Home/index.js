d53f758085a317df49d3cc7cb6cbb6b27d430ba9
SantosMaycon <mayconalves.santos14@gmail.com>
2021-02-20 21:41:45 -0300

    fix import

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
 
 const Main = () => {
-  document.querySelector("body").className = "gradiente"
+  document.querySelector("body").className = "gradiente";
 
   return (
-   <main className={styles.main}>
-     <div className={styles.container}>
+    <main className={styles.main}>
+      <div className={styles.container}>
         <div className={styles.info}>
-          <h1 className={styles.title}><span>Find</span> all your favorite <span>Pokemon</span></h1>
-          <h2 className={styles.subtitle}>You can know the type of Pokemon, its strengths, disadvantages and abilities</h2>
-          <a href="search" className={styles.button}>See pokemons</a>
+          <h1 className={styles.title}>
+            <span>Find</span> all your favorite <span>Pokemon</span>
+          </h1>
+          <h2 className={styles.subtitle}>
+            You can know the type of Pokemon, its strengths, disadvantages and
+            abilities
+          </h2>
+          <a href="search" className={styles.button}>
+            See pokemons
+          </a>
         </div>
         <div className={styles.img}></div>
-     </div>
-   </main>
-  )
-}
+      </div>
+    </main>
+  );
+};
 
-export default Main
+export default Main;
--- src/components/SearchPokemons/index.js
+++ src/components/SearchPokemons/index.js
@@ -1,68 +1,80 @@
-import React from 'react'
-import styles from './styles.module.css'
-import CardPokemon from './CardPokemon'
-import InputPokemon from './InputPokemon'
-import { getPokemon, orderPokemons } from '../../Utils/Util'
+import React from "react";
+import styles from "./styles.module.css";
+import CardPokemon from "./CardPokemon";
+import InputPokemon from "./InputPokemon";
+import { getPokemon, orderPokemons } from "../../Utils/Util";
 
 const SearchPokemons = () => {
-  document.querySelector("body").className = "normal"
+  document.querySelector("body").className = "normal";
 
-  const [pokemons, setPokemons] = React.useState([])
+  const [pokemons, setPokemons] = React.useState([]);
   const [urlList, setUrlList] = React.useState([]);
-  const [pages, setPages] = React.useState("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0")
+  const [pages, setPages] = React.useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=9&offset=0",
+  );
   const [next, setNext] = React.useState(null);
-  const [infinite, setInfinite] = React.useState(true)
+  const [infinite, setInfinite] = React.useState(true);
 
   React.useEffect(() => {
     fetch(pages)
-    .then( (response) => response.json()).then( (json) => { 
-      setUrlList(json.results.map((result) => result.url ))
-      setNext(json.next) 
-    })
-  },[pages])
-
-  React.useEffect(() => {
-    urlList.map((url) => fetch(url)
       .then((response) => response.json())
-      .then((json) => setPokemons((pokemons) => [...pokemons, getPokemon(json)]))
-    )
-  },[urlList])
+      .then((json) => {
+        setUrlList(json.results.map((result) => result.url));
+        setNext(json.next);
+      });
+  }, [pages]);
 
   React.useEffect(() => {
-    let wait = false
+    urlList.map((url) =>
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) =>
+          setPokemons((pokemons) => [...pokemons, getPokemon(json)]),
+        ),
+    );
+  }, [urlList]);
+
+  React.useEffect(() => {
+    let wait = false;
     function infiniteScroll() {
       if (infinite) {
-        const scroll = window.scrollY
-        const height = document.body.offsetHeight - window.innerHeight
-        if (scroll > height * 0.70 && !wait) {
-          wait = true
-          console.log("Inifite status:", infinite)
-          console.log("Next:", next)
-          setPages(next)
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+        if (scroll > height * 0.7 && !wait) {
+          wait = true;
+          console.log("Inifite status:", infinite);
+          console.log("Next:", next);
+          setPages(next);
           setTimeout(() => {
-            wait = false
-          }, 3000)
+            wait = false;
+          }, 2500);
         }
       }
     }
 
-    window.addEventListener('wheel', infiniteScroll)
-    window.addEventListener('scroll', infiniteScroll)
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
     return () => {
-      window.removeEventListener('wheel', infiniteScroll)
-      window.removeEventListener('scroll', infiniteScroll)
-    }
-  },[infinite, next])
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
+    };
+  }, [infinite, next]);
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <h3 className={styles.title}>890 <span>Pokemons</span> for you to choose your favorite</h3>
-        <InputPokemon setUrlList={setUrlList} setPokemons={setPokemons} setInfinite={setInfinite}/>
-        { pokemons && <CardPokemon pokemons={pokemons.sort(orderPokemons)} /> }
+        <h3 className={styles.title}>
+          890 <span>Pokemons</span> for you to choose your favorite
+        </h3>
+        <InputPokemon
+          setUrlList={setUrlList}
+          setPokemons={setPokemons}
+          setInfinite={setInfinite}
+        />
+        {pokemons && <CardPokemon pokemons={pokemons.sort(orderPokemons)} />}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default SearchPokemons
+export default SearchPokemons;
